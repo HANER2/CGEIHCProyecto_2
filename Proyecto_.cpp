08978cfdcbff50903f801fb731c372f9ab3edfd0
Juan Andrés Cruz <133611348+HANER2@users.noreply.github.com>
2023-05-27 18:25:28 -0600

    Update Proyecto_.cpp

--- Proyecto_.cpp
+++ Proyecto_.cpp
@@ -719,14 +719,6 @@ int main()
 		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		ESFERA1ESTRELLA_M.RenderModel();
-
-		//-------Modelo faro público.-------
-		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 50.0f));
-		model = glm::rotate(model, 90 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		FAROPUBLICO_M.RenderModel();
 		
 		//-------Circuito cerrado.-------
 		model = glm::mat4(1.0);
